0f771f692ce8544abf64ffb2e240d641b75e0e07
hzhigeng <hzhigeng@qq.com>
2017-02-18 12:52:14 +0800

    add access check for user/role page

--- um/admin/__init__.py
+++ um/admin/__init__.py
@@ -2,17 +2,25 @@ from flask.ext.admin import Admin, AdminIndexView
 from flask.ext.admin.contrib.sqla import ModelView
 from flask_security import Security, SQLAlchemyUserDatastore
 from db import db
+from flask_admin import helpers as admin_helpers
 from ..models import *
 from .views import AccessCheckView
 
 
 def set_up_admin(app, *args):
     admin = Admin(app, index_view=AdminIndexView(url='/um/admin'))
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Role, db.session))
 
-
-def set_up_security(app, *args):
-    # Setup Flask-Security
     user_datastore = SQLAlchemyUserDatastore(db, User, Role)
     security = Security(app, user_datastore)
+
+    admin.add_view(AccessCheckView(User, db.session))
+    admin.add_view(AccessCheckView(Role, db.session))
+
+    @security.context_processor
+    def security_context_processor():
+        return dict(
+            admin_base_template=admin.base_template,
+            admin_view=admin.index_view,
+            h=admin_helpers,
+            get_url=url_for
+        )
